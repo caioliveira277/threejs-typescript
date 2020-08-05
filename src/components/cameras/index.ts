8b2ce850ebd8e7ac4771ab651f39653d01163756
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2020-08-04 21:33:17 -0300

    feat: cube

--- src/components/cameras/index.ts
+++
@@ -1,7 +0,0 @@
-import { PerspectiveCamera } from "three";
-
-export class Camera extends PerspectiveCamera {
-  constructor(props: Omit<PerspectiveCamera, "position">) {
-    super(props.fov, props.aspect, props.near, props.far);
-  }
-}
--- src/index.ts
+++ src/index.ts
@@ -2,41 +2,29 @@
 import "./assets/sass/styles.scss";
 
 import * as THREE from "three";
-import { Camera } from "./components/cameras";
 
-const camera = new Camera({
-  fov: 70,
-  aspect: window.innerWidth / window.innerHeight,
-  near: 0.01,
-  far: 10,
-});
+const camera = new THREE.PerspectiveCamera(
+  70,
+  window.innerWidth / window.innerHeight,
+  0.01,
+  10
+);
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+const material = new THREE.MeshNormalMaterial();
+const mesh = new THREE.Mesh(geometry, material);
+
 camera.position.z = 1;
+scene.add(mesh);
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-var scene, renderer;
-var geometry, material, mesh;
+document.body.appendChild(renderer.domElement);
 
-init();
-animate();
-
-function init() {
-  scene = new THREE.Scene();
-
-  geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-  material = new THREE.MeshNormalMaterial();
-
-  mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
-
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-}
-
-function animate() {
+(function animate() {
   requestAnimationFrame(animate);
-
   mesh.rotation.x += 0.01;
   mesh.rotation.y += 0.02;
-
   renderer.render(scene, camera);
-}
+})();
